f8121eeab13782f4447cd2596d6455d3affd074c
Michael Rubin <mhr@neverthere.org>
2024-01-07 19:08:23 -0800

    Adding the plex certificate steps

--- glory_cert.sh
+++ glory_cert.sh
@@ -6,3 +6,8 @@ certbot --config-dir=. --work-dir=. --logs-dir=. \
 	-d glory.neverthere.org \
 	--preferred-challenges dns \
 	certonly
+
+# For Plex
+# https://www.nodinrogers.com/post/2022-06-01-customer-ssl-cert-in-plex/
+# openssl pkcs12 -export -out glory.neverthere.org.pfx -inkey ./privkey.pem -in ./cert.pem -name glory.neverthere.org -passout pass:""
+
