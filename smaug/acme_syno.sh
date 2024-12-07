7a3a2203e5984f9f0f54e9647f87b6f6b5950cf6
Michael Rubin <mhr@neverthere.org>
2024-12-07 14:31:41 -0800

    Using the acme.sh script for synonology certs

---
+++ smaug/acme_syno.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+
+echo "Add env info first"
+exit 1
+
+export CF_Token=""
+export CF_Email="mhr@neverthere.org"
+export SYNO_PASSWORD=
+export SYNO_USERNAME=
+
+./acme.sh/acme.sh --register-account  -m mhr@neverthere.org --server zerossl
+./acme.sh/acme.sh --server zerossl --issue -d smaug.neverthere.org --dns dns_cf
+./acme.sh/acme.sh --force --debug --issue --dns dns_cf -d neverthere.org -d 'smaug.neverthere.org'
+./acme.sh/acme.sh --deploy --deploy-hook synology_dsm -d smaug.neverthere.org
